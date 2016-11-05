720f4e3763a86aaf29e022771510ffaf73ca3cd9
Khanh TO <suzhiqing197@gmail.com>
2016-11-05 18:15:44 +0700

    Adding WebChat.IdentityServer asp.net core project with basic configurations

---
+++ WebChat.IdentityServer/Config.cs
@@ -0,0 +1,41 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace WebChat.IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<Scope> GetScopes()
+        {
+            return new List<Scope>
+            {
+                StandardScopes.OpenId,
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "webchat",
+
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                    // secret for authentication
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = new List<string>
+                    {
+                        StandardScopes.OpenId.Name
+                    }
+                }
+            };
+        }
+    }
+}
